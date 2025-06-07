0bc9358965e47c1e6d1a6f00990efc14c43cb5b8
JakubWlochTuring <jakub.w@turing.com>
2025-06-07 07:47:43 +0200

    More comments

--- models/task.py
+++ models/task.py
@@ -16,3 +16,14 @@ class Task(Base):
     
     # Relationship: Many tasks belong to one user
     user = relationship("User", back_populates="tasks")
+
+
+@router.post("/api/users/") 
+def create_user_bad(user_data: UserCreate, db: Session = Depends(get_db)):
+    # HTTP + Logika biznesowa + dostęp do danych = MESS
+    if not user_data.username:  # Logika biznesowa pośród routingu
+        raise HTTPException(400, "Username required")
+    user = User(username=user_data.username, email=user_data.email)
+    db.add(user)  # Bezpośrednie połączenie do DB w routingu
+    db.commit()
+    return user
